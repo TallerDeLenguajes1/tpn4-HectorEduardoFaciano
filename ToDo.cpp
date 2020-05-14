2c270eb7f4729c73ae148dbc3bb358f6a6be58eb
HectorEduardoFaciano <edu21.ef@gmail.com>
2020-05-14 17:22:56 -0300

    cambios tp5

--- ToDo.cpp
+++ ToDo.cpp
@@ -3,11 +3,16 @@
 #include <string.h>
 #include <time.h>
 
-struct Tarea{
+typedef struct Tarea{
     int TareaID;
     char * Descripcion;
     int Duracion;
-}; 
+}Tarea;
+
+typedef struct Nodo{
+    Tarea T​;
+    Nodo * Siguiente; // Siguiente Nodo.
+}Nodo;
 
 void cargar_tareas(Tarea ** tareas_pendientes, int cantidad_tareas);
 void separar_tareas(Tarea ** tareas_pendientes, Tarea ** tareas_realizadas, int cantidad_tareas);
@@ -15,6 +20,15 @@ void mostrar_tareas(Tarea ** tareas, int cantidad_tareas);
 Tarea BusquedaPorID(Tarea ** tareas_pendientes, Tarea ** tareas_realizadas, int cantidad_tareas);
 Tarea BusquedaPorPalabra(Tarea ** tareas_pendientes, Tarea ** tareas_realizadas, int cantidad_tareas);
 void Busqueda(Tarea ** tareas_pendientes, Tarea ** tareas_realizadas, int cantidad_tareas);
+
+Nodo *CrearNodo(Tarea *tareas_pendientes){
+    Nodo *nodo = (Nodo *) malloc(sizeof(Nodo));
+    nodo->T​.TareaID = tareas_pendientes->TareaID; 
+    nodo->T​.Descripcion = tareas_pendientes->Descripcion;
+    nodo->T​.Duracion = tareas_pendientes->Duracion;
+    nodo->Siguiente = NULL;
+    return nodo;
+}
 
 int main(){
     srand(time(NULL));
@@ -27,13 +41,26 @@ int main(){
     Tarea **tareas_realizadas = (Tarea **) malloc(sizeof(Tarea *) * cantidad_tareas);
     Tarea **tareas_pendientes = (Tarea **) malloc(sizeof(Tarea *) * cantidad_tareas);
     
+    CrearNodo(*tareas_pendientes);
     cargar_tareas(tareas_pendientes, cantidad_tareas);
     separar_tareas(tareas_pendientes, tareas_realizadas, cantidad_tareas);
     
     printf("---------------TAREAS REALIZADAS---------------\n\n");
-    mostrar_tareas(tareas_realizadas, cantidad_tareas);
+    Nodo *nodo_realizadas = (Nodo *)malloc(sizeof(Nodo) * cantidad_tareas);
+    while (nodo_realizadas != NULL){
+        printf("Tarea ID: %d\n", nodo_realizadas->T​.TareaID);
+        printf("Descripcion: \"%s\"\n", nodo_realizadas->T​.Descripcion);
+        printf("Duracion: %d\n\n", nodo_realizadas->T​.Duracion);
+    }
+    //mostrar_tareas(tareas_realizadas, cantidad_tareas);
     printf("---------------TAREAS PENDIENTES---------------\n\n");
-    mostrar_tareas(tareas_pendientes, cantidad_tareas);
+    Nodo *nodo_pendientes = (Nodo *)malloc(sizeof(Nodo) * cantidad_tareas);
+    while (nodo_pendientes != NULL){
+        printf("Tarea ID: %d\n", nodo_pendientes->T​.TareaID);
+        printf("Descripcion: \"%s\"\n", nodo_pendientes->T​.Descripcion);
+        printf("Duracion: %d\n\n", nodo_pendientes->T​.Duracion);
+    }
+    //mostrar_tareas(tareas_pendientes, cantidad_tareas);
     
     Tarea Buscar1;
     printf("---------------BUSQUEDA POR ID---------------\n\n");
@@ -47,18 +74,19 @@ int main(){
 }
 
 void cargar_tareas(Tarea ** tareas_pendientes, int cantidad_tareas){
+    Nodo *nodo_pendientes = CrearNodo(*tareas_pendientes);
     for (int i = 0; i < cantidad_tareas; i++)
     {
         char descripcion[60];
         tareas_pendientes[i] = (Tarea *) malloc(sizeof(Tarea));
         printf("Tarea #%d\n", i+1);
-        tareas_pendientes[i]->TareaID = i+1;
+        nodo_pendientes->T​.TareaID = i+1;
         printf("Descripcion: ");
         fflush(stdin);
         gets(descripcion);
-        tareas_pendientes[i]->Descripcion = (char *)malloc(sizeof(descripcion) * (strlen(descripcion)));
-        strcpy(tareas_pendientes[i]->Descripcion, descripcion);
-        tareas_pendientes[i]->Duracion = rand() % 91 + 10;;
+        nodo_pendientes->T​.Descripcion = (char *)malloc(sizeof(descripcion) * (strlen(descripcion)));
+        strcpy(nodo_pendientes->T​.Descripcion, descripcion);
+        nodo_pendientes->T​.Duracion = rand() % 91 + 10;;
         system("cls");
         tareas_pendientes++;
     }   
@@ -66,20 +94,34 @@ void cargar_tareas(Tarea ** tareas_pendientes, int cantidad_tareas){
 
 void separar_tareas(Tarea **tareas_pendientes, Tarea **tareas_realizadas, int cantidad_tareas){
     int aux;
+    Nodo *nodo_pendientes = CrearNodo(*tareas_pendientes);
     for (int i = 0; i < cantidad_tareas; i++){
-        printf("Tarea ID: %d\n", tareas_pendientes[i]->TareaID);
-        printf("Descripcion: \"%s\"\n", tareas_pendientes[i]->Descripcion);
-        printf("Duracion: %d\n\n", tareas_pendientes[i]->Duracion);
+        printf("Tarea ID: %d\n", nodo_pendientes->T​.TareaID);
+        printf("Descripcion: \"%s\"\n", nodo_pendientes->T​.Descripcion);
+        printf("Duracion: %d\n\n", nodo_pendientes->T​.Duracion);
         do{
             printf("Esta tarea esta realizada?\n");
             printf("Presone 1(SI) o 0(NO): ");
             scanf("%d",&aux);
         } while (aux != 0 && aux != 1);
-        
+
+        Nodo *nodo_pendientes = CrearNodo(*tareas_pendientes);
+        Nodo *nodo_realizadas = CrearNodo(*tareas_realizadas);
         if (aux == 1){
             tareas_realizadas[i] = (Tarea *) malloc(sizeof(Tarea));
-            tareas_realizadas[i] = tareas_pendientes[i];
+        //
+            if (tareas_realizadas[i] == NULL){
+                nodo_realizadas->T​.TareaID = nodo_pendientes->T​.TareaID;
+                nodo_realizadas->T​.Descripcion = nodo_pendientes->T​.Descripcion;
+                nodo_realizadas->T​.Duracion = nodo_pendientes->T​.Duracion;
+            }else{
+                while (nodo_pendientes->Siguiente){
+                    nodo_pendientes = nodo_pendientes->Siguiente;
+                } 
+                nodo_pendientes->Siguiente = nodo_pendientes;
+            }
             tareas_pendientes[i] = NULL;
+        //
         }else{
             tareas_realizadas[i] = NULL;
         }
